0ab2291234c91c1c97f91b5ebf7299df6a17c977
Abu Siam <siamcse01@gmail.com>
2022-12-05 18:15:49 +0600

    severside add to todo.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,8 @@ import { connectToDatabase } from '../../util/mongodb';
 
 export default async function handler(req, res) {
   const {db} = await connectToDatabase();
-  const data = db.collection("todos").insertOne({email:"siamcse01@gmail.com"});
+
+  const data = db.collection("todos").find({}).toArray();
+
   res.status(200).json({ name: 'John Doe'},data)
 }
