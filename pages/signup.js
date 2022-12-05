7700ceecaed7e380706c761dd08a34778adcd74b
Abdur Rahman Emon <aremon86851@gmail.com>
2022-12-05 16:31:09 +0600

    signup page added

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,38 @@
 
+
 const signup = () => {
     return (
-        <div>
-            This is signup
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col gap-5 lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <img src='./signup.png' alt="hello" className="w-3/4" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="full-name" className="input input-bordered italic text-xs" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered italic text-xs" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered italic text-xs" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
